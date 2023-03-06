a0432f1bb6590c0d11f01e5fbc38f911ca1856a6
Issaac Pettengill <issaac@Issaacs-iMac.lan>
2023-03-05 20:52:20 -0500

    final drafts

---
+++ destructuringExercise.js
@@ -0,0 +1,43 @@
+// what does the code print when it is console.logged??
+
+console.log(numPlanets);  8
+console.log(yearNeptuneDiscovered);  1846
+
+
+//what does the code return?
+
+console.log(discoveryYears);  //{yearNeptuneDIscovered: 1846, yearMarsDiscovered: 1659};
+
+//what does the code return??
+
+getUserData({firsteName: "Alejandro", favoriteColor: "purple"});  //Your name is Alejandro and you like purple.
+getUserData({firsteName: "Mellisa"});  //Your name is Mellisa and you like green.
+getUserData({});  //your name is undefined and you like green
+
+//what doe the code return??
+
+console.log(first);  //Maya
+console.log(second);  //marisa
+console.log(third);  //Chi
+
+//what does the code return??
+
+console.log(raindrops);  // "raindrops on rose"
+console.log(whiskers);  // "whiskers on kittens"
+console.log(aFewOfMyFavoriteThings);  //["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+//what does the code return??
+
+console.log(numbers);  //[10, 30, 20]
+
+//update the code
+
+const {a,b} = obj.numbers
+
+//update the code
+
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+//write a function to return the race results
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
